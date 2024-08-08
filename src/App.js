de43f718687bca1d1fd5735be1d4bd9010ecaf88
Abhishek Godse <abhigodse9556@gmail.com>
2024-08-08 21:36:54 +0530

    New alert feature is added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import './App.css';
+import About from './components/About';
+import Alert from './components/Alert';
 import './components/Main.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
-import Main from './components/main';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import React, {useState} from 'react';
 function App() {
+  
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+
+  const [mode, setMode] = useState('light')
+  const [modeTxt, setModeTxt] = useState('Enable dark mode')
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      setModeTxt('Enable light mode')
+      document.body.style.backgroundColor = "#1b1e21"
+      document.body.style.color = "white"
+      showAlert("Dark mode has been enabled", "Success")
+    }
+    else{
+      setMode('light')
+      setModeTxt('Enable dark mode')
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = "black"
+      showAlert("Light mode has been enabled", "Success")
+    }
+  }
+
+
+      
   return (
     <>
-      <Main></Main>
-      <Navbar title='TextUtils'/>
+      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} modeTxt={modeTxt}/>
+      <Alert alert={alert}/>
       <div className="container">
-        <TextArea heading="Enter the Text to Analyze Below"/>
+        <TextArea heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert}/>
       </div>
+      <About mode={mode}/>
     </>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -7,46 +7,71 @@ export default function TextArea(props) {
     const handleOnclickUpper = ()=>{
         let newtext = text.toUpperCase()
         setText(newtext)
+        props.showAlert("Converted to Uppercase!", "success")
     }
     const handleOnclickLower = ()=>{
         let newtext = text.toLowerCase()
         setText(newtext)
+        props.showAlert("Converted to Lowercase!", "success")
     }
     const handleOnclickCapitalize = () => {
         let text1 = text.toLowerCase()
         let newText = text1.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
         setText(newText);
+        props.showAlert("Converted to Capitalize!", "success")
     }
     const handleOnclickReverse = () => {
         let str="";
         for (let index = text.length-1; index >=0; index--) {
         str+= text[index];
         }
-        
+        props.showAlert("Your Text is Reversed!", "success")
         return setText(str);
     }
     const handleOnclickSpeak = () => {
         let msg = new SpeechSynthesisUtterance();
         msg.text = text;
         window.speechSynthesis.speak(msg);
+        props.showAlert("Speaking started!", "success")
+        msg.onend = () => {
+            props.showAlert("Speaking completed!", "success");
+        }
+    }
+    const handleCopy = () => {
+        let txt = document.getElementById('textBox');
+        txt.select();
+        navigator.clipboard.writeText(txt.value);
+        props.showAlert("Text is Copied to Clipboard!", "success")
+    }
+    const handleExtraSpaces = () => {
+        let txt = text.split(/[ ]+/)
+        setText(txt.join(" "))
+        props.showAlert("Extra Spaces have been removed!", "success")
     }
     const handleOnclickClear = ()=>{
         let newtext = ""
         setText(newtext)
+        props.showAlert("Text Cleared!", "success")
     }
     const [text, setText] = useState('')
+    let myStyle = {
+        backgroundColor: 'white',
+        color: "black"
+    }
     return (
         <>
             <div className="container">
                 <div className="mb-3">
                     <h1 className='my-3'>{props.heading}</h1>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="15" value={text} onChange={handleOnchange} placeholder='Enter your text here'></textarea>
+                    <textarea className={`form-control bg-${props.mode} text-${props.mode==='light'?'dark':'light'}`} id="textBox" rows="15" value={text} onChange={handleOnchange} placeholder='Enter your text here' style={myStyle}></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickUpper}>Upper Case</button>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickLower}>Lower Case</button>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickCapitalize}>Capitalize</button>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickReverse}>Reverse</button>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickSpeak}>Speak</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy to Clipboard</button>
                 <button className="btn btn-primary mx-1" onClick={handleOnclickClear}>Clear</button>
             </div>
             <div className="container my-3">
@@ -56,7 +81,7 @@ export default function TextArea(props) {
                 <h4>Average time required to read your text</h4>
                 <p>{(text.split(" ").length) * 0.008} Minutes</p>
                 <h4>Preview</h4>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Enter something in the text box above to preview it here'}</p>
             </div>
         </>
     )
